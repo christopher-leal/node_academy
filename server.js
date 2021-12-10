61938b67f3a1dee95b50d15a7d1a679c70aba248
Christopher Leal Gomez <christopher.leal@globant.com>
2021-12-10 13:42:01 -0600

    dotenv added to the project and root route created

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+require('dotenv').config()
 
 const app = express()
-
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(helmet())
-
+app.use(express.json())
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server listening port ${PORT}`)
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Server online'
+  })
 })
+
+app.listen(PORT, () => console.log(`Server listening port ${PORT}`))
